52c59656f89cb336fcbe1f3c7623ac53079dfee6
rpp0041 <rpp0041@alu.ubu.es>
2018-11-21 16:20:09 +0100

    Update GetPublicationsScopus
    Realizado cambios en el sistema de almacenamiento de los datos.
    
    Ahora son Almacenados en formato Bibtex.

--- GetPublicationsScopus.py
+++ GetPublicationsScopus.py
@@ -1,5 +1,20 @@
 from pyscopus import Scopus # Import library that will help us in the search
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
 
+def GetAU(id): # Function that will parse author id into real name.
+    string=''
+    lg=len(author_pub['author'][id])
+    for i in range(0,lg-1):
+        query = 'au-id(%s)'%author_pub['author'][id][i]
+        a=scopus.search_author(query,10)
+        string+=(a['name'][0])
+        string+=' and '
+    query = 'au-id(%s)'%author_pub['author'][id][lg-1]
+    a=scopus.search_author(query,10)
+    string+=(a['name'][0])
+    return string
+   
 key ='a3ddc2b8df64bc16774266fb842c0365' # Key gived by Elsevier to acces their api 
 scopus = Scopus(key)
 
@@ -9,11 +24,48 @@ author_pub= scopus.search_author_publication(author_id,10000) # int number indic
 															  # (pending of improve)
 
 
-titles = author_pub['title']	# list with all the author´s publications
-citation = author_pub['citation_count'] # list with number of citations for each publication
+############################################################################
+# Modifications on Internal data structure to fit with Bibtex Structure
+author_pub['pages']=author_pub['page_range']
+author_pub.pop('page_range')
 
-## Write data to txt File
-file = open('testfileScopus.txt','w') 
-for i in range(0,len(titles)):
-    file.write(titles[i]+' , '+str(citation[i])+'\n')
-file.close()
+author_pub['author']=author_pub['authors']
+author_pub.pop('authors')
+
+author_pub['ID']=author_pub['scopus_id']
+author_pub.pop('scopus_id')
+
+author_pub['ENTRYTYPE']=author_pub['aggregation_type']
+author_pub.pop('aggregation_type')
+
+author_pub['url']=author_pub['full_text']
+author_pub.pop('full_text')
+
+author_pub['note']=author_pub['publication_name']
+author_pub.pop('publication_name')
+
+listOfKeys=list(author_pub.keys()) # Remove some Keys that will be added later with some modifications
+listOfKeys.remove('author')
+listOfKeys.remove('citation_count')
+listOfKeys.remove('affiliation')
+############################################################################
+
+db = BibDatabase()
+numPub=len(author_pub['title']) # Number of Publications retrieved
+writer = BibTexWriter()
+
+
+for i in range(0,numPub):
+    bib=dict()
+    bib['author']=GetAU(i)
+    bib['citation_count']=str(author_pub['citation_count'][i])
+    for x in listOfKeys:
+        if author_pub[x][i] is not None:
+            if x is 'affiliation':
+                bib['affiliation']=author_pub['affiliation'][i][0]['name']
+            else:
+                bib[x]=author_pub[x][i]
+    db.entries=[bib]
+
+    with open('bibtexScopus.bib', 'a' , encoding='utf-8') as bibfile:
+        bibfile.write(writer.write(db))

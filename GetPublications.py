a62ecacccca38a8ca2b4250e391411c6e3b8a93f
rpp0041 <rpp0041@alu.ubu.es>
2018-12-03 20:35:53 +0100

    Minor fix
    Mejora en la extracción de datos de Google Scholar, en el campo <abstract>, el cual no era correctamente parseado por la librería. Se ha implementado una función para ello

--- GetPublications.py
+++ GetPublications.py
@@ -2,6 +2,21 @@ import scholarly # Non official Api for Google Scholar
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 
+def ParseAbstract(i):
+    abstract=str(i.bib['abstract'])
+    abstract=abstract.split('class="gsh_csp">')
+    if len(abstract)<2:
+        abstract=abstract[0].split('class="gsh_small">')
+        if len(abstract)<2:
+            abstract=abstract[0].split('"gsc_vcd_descr">')[1]
+        else:
+            abstract=abstract[1]
+    else:
+        abstract=abstract[1]
+    abstract=abstract.split('</div>')[0]
+    return abstract
+
+
 authorInput= input('Nombre del Autor: ')
 
 search_query = scholarly.search_author(authorInput)
@@ -12,21 +27,21 @@ db = BibDatabase()
 
 cont =0
 writer = BibTexWriter()
-for i in author_pub	:
-	i.fill()
-
 for i in author_pub:
-	## Need modifications for BibTextWriter 
+    i.fill()
+    ## Need modifications for BibTextWriter 
     if 'journal' in i.bib.keys():
         i.bib['ENTRYTYPE']='article'
     else: 
         i.bib['ENTRYTYPE']='book'
     i.bib['ID']=str(cont)
     if 'abstract' in i.bib.keys():
-        i.bib['abstract']=str(i.bib['abstract'])
+        abstract=ParseAbstract(i)
+        i.bib['abstract']=abstract
     if 'year' in i.bib.keys():
         i.bib['year']=str(i.bib['year'])
     db.entries=[i.bib]
     cont+=1
     with open('bibtex.bib', 'a' , encoding='utf-8') as bibfile:
         bibfile.write(writer.write(db))
+    
